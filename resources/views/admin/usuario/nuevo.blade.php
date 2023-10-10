7e857d69c0c34778045f14610ecdda3baae250e2
Grover2m <jhon_grx@hotmail.com>
2023-10-10 16:44:48 -0400

    JetStream

--- resources/views/admin/usuario/nuevo.blade.php
+++ resources/views/admin/usuario/nuevo.blade.php
@@ -1,34 +1,49 @@
-<h1>Nuevo Usuario</h1>
+@extends("layouts.admin")
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+@section("titulo","Nuevo Usuario")
+
+@section("contenido")
+
+<div class="card card-primary card-outline">
+    <div class="card-header">
+        <h5 class="m-0 float-left">Lista de Usuarios</h5>
+           </div>
+    <div class="card-body">
+
+       <!-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif -->
+
+        <form action="{{route('usuario.store')}}" method="post">
+            @csrf
+            <label for="">Ingrese Nombre</label>
+            <input type="text" name="name" value=" {{old('name')}}" class= "form-control @error('name')is-invalid @enderror()">
+            @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <br>
+            <label for="">Ingrese Correo</label>
+            <input type="email" name="email" value="{{old('email')}}" class= "form-control @error('email')is-invalid @enderror()">
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <br>
+            <label for="">Ingrese Password</label>
+            <input type="password" name="password" value="{{old('password')}}" class= "form-control @error('password')is-invalid @enderror()">
+            <br>
+            @error('password')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+            <input type="submit" value="Guardar Usuario" class= "btn btn-primary">
+        </form>
+    </div>
+
 </div>
-@endif
 
-<form action="{{route('usuario.store')}}" method="post">
-    @csrf
-    <label for="">Ingrese Nombre</label>
-    <input type="text" name="name" value=" {{old('name')}}">
-    @error('name')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <br>
-    <label for="">Ingrese Correo</label>
-    <input type="email" name="email" value="{{old('email')}}">
-    @error('email')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <br>
-    <label for="">Ingrese Password</label>
-    <input type="password" name="password" value="{{old('password')}}">
-    <br>
-    @error('password')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
-    <input type="submit" value="Guardar Usuario">
-</form>
+@endsection
